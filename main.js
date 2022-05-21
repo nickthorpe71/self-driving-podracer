4b9f4c4975c583068465e16db3a0a08ebe849958
Nick Thorpe <nickthorpe71@gmail.com>
2022-05-21 11:10:06 -0500

    add camera follow to car

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById("mainCanvas");
-canvas.width = 200;
+canvas.width = 400;
 
 const ctx = canvas.getContext("2d");
 const road = createRoad(canvas.width / 2, canvas.width * 0.9);
-const podracer = createPodracer(100, 100, 30, 50);
+const podracer = createPodracer(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -12,7 +12,13 @@ function animate() {
 
     // when you resize the canvas it also clears the canvas
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -podracer.y + canvas.height * 0.7);
+
     road.draw(ctx);
     podracer.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -1,23 +1,44 @@
-const createRoad = (x, width, laneCount = 3) => {
+const createRoad = (x, width, laneCount = 6) => {
     const infinity = 1000000000;
+
+    const left = x - width / 2;
+    const right = x + width / 2;
+    const top = -infinity;
+    const bottom = infinity;
+
+    const topLeft = { x: left, y: top };
+    const topRight = { x: right, y: top };
+    const bottomLeft = { x: left, y: bottom };
+    const bottomRight = { x: right, y: bottom };
+
+    const borders = [
+        [topLeft, bottomLeft],
+        [topRight, bottomRight]
+    ];
 
     const road = {
         x,
         width,
         laneCount,
 
-        left: x - width / 2,
-        right: x + width / 2,
+        left,
+        right,
+        top,
+        bottom,
 
-        top: -infinity,
-        bottom: infinity
+        borders,
+    }
+
+    const getLaneCenter = (laneIndex) => {
+        const laneWidth = road.width / road.laneCount;
+        return road.left + laneWidth / 2 + Math.min(laneIndex, road.laneCount - 1) * laneWidth;
     }
 
     const draw = (ctx) => {
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = "#cac5ba";
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = "#ece7dc";
 
-        for (let i = 0; i <= road.laneCount; i++) {
+        for (let i = 1; i <= road.laneCount - 1; i++) {
             const x = lerp(
                 road.left,
                 road.right,
@@ -25,13 +46,24 @@ const createRoad = (x, width, laneCount = 3) => {
             );
 
             ctx.beginPath();
+            ctx.setLineDash([20, 20]);
             ctx.moveTo(x, road.top);
             ctx.lineTo(x, road.bottom);
             ctx.stroke();
         }
+
+        ctx.setLineDash([]);
+
+        road.borders.forEach(border => {
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
     }
 
     road.draw = draw;
+    road.getLaneCenter = getLaneCenter;
 
     return road;
 }
